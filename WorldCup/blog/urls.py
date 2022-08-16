321054e348598eaed887c62787046755fe2cb458
Nelson Salazar <urugrafico@gmail.com>
2022-08-16 00:34:14 -0300

    En app blog, view inicio creada, template en html básico

---
+++ WorldCup/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog.views import (
+    inicio,
+)
+
+urlpatterns = [
+    path('', inicio, name="Inicio"),
+]
--- WorldCup/blog/views.py
+++ WorldCup/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(self):
+    return render(self, "inicio.html")
