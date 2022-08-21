0d2a3d035fc6108b6f29d9f53725a8ac1bd698aa
Nelson Salazar <urugrafico@gmail.com>
2022-08-21 10:12:47 -0300

    Carpeta static creada, html base creado, página de inicio creada con herencia

--- WorldCup/WorldCup/urls.py
+++ WorldCup/WorldCup/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Paths del blog
+    # Paths de noticias
     path('', include('noticias.urls')),
     # Paths del admin
     path('admin/', admin.site.urls),
