b6681cac3f32556898ff43156ebbe8f5d24c6427
Nelson Salazar <urugrafico@gmail.com>
2022-08-27 01:49:49 -0300

    Registro de usuario creado y funcionando

--- WorldCup/WorldCup/urls.py
+++ WorldCup/WorldCup/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Paths de autenticación
     path('accounts/', include('django.contrib.auth.urls')), # urls de django
+    path('accounts/', include('registration.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
